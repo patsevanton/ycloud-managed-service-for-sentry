179d7d82ab80cd5f3adc3ad8a48f8e15bfcc75c5
Anton Patsev <patsev.anton@gmail.com>
2024-11-17 19:35:59 +0600

    add k8s

--- main.tf
+++ main.tf
@@ -31,3 +31,37 @@ module "network" {
   create_vpc         = true
   create_nat_gateway = true
 }
+
+
+module "kube" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-kubernetes.git?ref=v1.0.0"
+
+  network_id = module.network.vpc_id
+
+  name = "k8s-sentry"
+  master_version = 1.27
+
+  service_account_id      = module.iam_accounts.id
+  node_service_account_id = module.iam_accounts.id
+
+  master_locations = [
+    {
+      zone      = "ru-central1-a"
+      subnet_id = module.network.private_subnets_ids[0]
+    }
+  ]
+
+  node_groups = {
+    "default" = {
+      nat    = true
+      cores  = 2
+      memory = 8
+      fixed_scale = {
+        size = 3
+      }
+    }
+  }
+
+  depends_on = [module.iam_accounts]
+
+}
